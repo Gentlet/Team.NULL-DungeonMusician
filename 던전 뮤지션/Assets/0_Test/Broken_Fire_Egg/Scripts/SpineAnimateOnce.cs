c68c8e0fcd30f29f405a33676ad33f4c132162c5
broken-fire-egg <6716000@naver.com>
2018-07-12 21:21:47 +0900

    스파인 +이펙트 작업좀 함

---
+++ 던전 뮤지션/Assets/0_Test/Broken_Fire_Egg/Scripts/SpineAnimateOnce.cs
@@ -0,0 +1,22 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpineAnimateOnce : MonoBehaviour {
+
+    public SkeletonGraphic SG;
+    private void Start()
+    {
+        SG = GetComponent<SkeletonGraphic>();
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(WaitDie());
+    }
+    IEnumerator WaitDie()
+    {
+        yield return new WaitForSpineAnimationComplete(SG.AnimationState.SetAnimation(0, "eft_0", false));
+        gameObject.SetActive(false);
+    }
+}
