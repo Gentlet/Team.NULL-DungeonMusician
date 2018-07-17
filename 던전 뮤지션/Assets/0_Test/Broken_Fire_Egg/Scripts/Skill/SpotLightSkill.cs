ef1ae27c3f4d7e2b49b909941aa7f9760586feaa
broken-fire-egg <6716000@naver.com>
2018-07-17 21:03:34 +0900

    잡다

---
+++ 던전 뮤지션/Assets/0_Test/Broken_Fire_Egg/Scripts/Skill/SpotLightSkill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightSkill : ObjectStatus {
+
+    public bool isactive;
+
+
+    public void ParamUpgrade()
+    {
+        param++;
+    }
+    public void Active()
+    {
+        isactive = true;
+        StartCoroutine(Acting());
+    }
+    public IEnumerator Acting()
+    {
+        yield return new WaitForSeconds(param);
+        isactive = false;
+    }
+
+
+}
