c48633a8e50da5563f3bb4395132d341b0d8f14e
broken-fire-egg <6716000@naver.com>
2018-08-10 14:17:19 +0900

    타이틀씬 완성(씬 넘기기 X)

--- 던전 뮤지션/Assets/0_Test/Broken_Fire_Egg/Scripts/SpineAnimateOnce.cs
+++ 던전 뮤지션/Assets/0_Test/Broken_Fire_Egg/Scripts/SpineAnimateOnce.cs
@@ -8,18 +8,40 @@ public class SpineAnimateOnce : MonoBehaviour {
     public SkeletonGraphic SG;
     public SkeletonAnimation SA;
     public bool IsCancas;
+    public bool loof;
     public string eftName;
+    public float delay; 
     private void OnEnable()
     {
         if (IsCancas)
+            Invoke("SCSG", delay);
+        else
+            Invoke("SCSA", delay);
+    }
+    public void SCSG()
+    {
+        if (loof)
+        {
+            SG.AnimationState.SetAnimation(0, eftName, true);
+        }
+        else
             StartCoroutine(WaitDieSG());
+    }
+    public void SCSA()
+    {
+        if (loof)
+        {
+            SA.AnimationState.SetAnimation(0, eftName, true);
+
+        }
         else
             StartCoroutine(WaitDieSA());
-       
     }
+
+
+
     IEnumerator WaitDieSG()
     {
-
         if(eftName == "")
             yield return new WaitForSpineAnimationComplete(SG.AnimationState.SetAnimation(0, "eft_0", false));
         else
