eb3ffcca898c15b4bd87068cb7bd3c1eaa5ef875
brok3n <limesaru@gmail.com>
2018-08-13 10:26:44 +0900

    메인메뉴 다만듬 ㅋㅋ

---
+++ 던전 뮤지션/Assets/0_Test/Broken/Scripts/UI/InfiniteScroll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteScroll : MonoBehaviour
+{
+    public float speed;
+    public float widthSize;
+    public RectTransform[] childs;
+
+    private Vector3 moveForce;
+
+    private void Start()
+    {
+        moveForce = new Vector3(speed, 0f, 0f);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < childs.Length; i++)
+        {
+            //RectTransformUtility.PixelAdjustRect(childs[i], canvas);
+            //Debug.Log(i + " : " + childs[i].localPosition.x);
+            childs[i].position += (moveForce * Time.deltaTime);
+
+            if (childs[i].transform.localPosition.x > 1080f)
+            {
+                if (i == 0)
+                    childs[i].localPosition = childs[childs.Length - 1].localPosition - new Vector3(widthSize, 0f);
+                else
+                    childs[i].localPosition = childs[i - 1].localPosition - new Vector3(widthSize, 0f);
+            }
+        }
+    }
+}
