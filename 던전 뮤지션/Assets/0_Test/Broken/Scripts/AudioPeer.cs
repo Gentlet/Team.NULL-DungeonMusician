c3c9659cd24e6e3f32fe3c18993d633a8f600500
brok3n <limesaru@gmail.com>
2018-08-21 14:52:01 +0900

    게임화면 이미지들 바꿈

---
+++ 던전 뮤지션/Assets/0_Test/Broken/Scripts/AudioPeer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource _audioSource;
+
+    public static float[] _samples = new float[4096];
+    public static float[] _freqBand = new float[12];
+
+    public float maxScale;
+
+    private void Start()
+    {
+        _audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    private void Update()
+    {
+        GetAudioSpectrum();
+        MakeFrequenctBands();
+    }
+
+    void GetAudioSpectrum()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+    void MakeFrequenctBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 12; i++)
+        {
+            float average = 0f;
+            int sampleCount = (int)Mathf.Pow(2, i)/* * 2*/;
+
+            //if (i == 13)
+            //    sampleCount += 2;
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+
+            if (_freqBand[i] > maxScale)
+                _freqBand[i] = maxScale;
+        }
+    }
+}
